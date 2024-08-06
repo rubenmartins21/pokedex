5a73f808812d121a2b5a6f056f95bd5d46d13468
rubenmartins21 <rubenmartins463@gmail.com>
2024-08-06 22:40:22 -0100

    feat: Add filter section

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,11 @@
-import { Box, IconButton, InputAdornment, InputBase } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import TuneIcon from "@mui/icons-material/Tune";
 import usePokemon from "../../hooks/usePokemon";
@@ -11,6 +17,10 @@ import { useTranslation } from "react-i18next";
 
 const Search: React.FC = () => {
   const { t } = useTranslation();
+
+  const [showFilterArea, setShowFilterArea] = useState<boolean>(false);
+
+  const [filterSection, setFilterSection] = useState<string>("type");
 
   const allPokemons = useSelector(
     (state: { pokemons: IPokemonInitialStates }) => state.pokemons.allPokemons
@@ -27,89 +37,147 @@ const Search: React.FC = () => {
   }, [allPokemons]);
 
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        marginBottom: "40px",
-      }}
-    >
+    <>
       <Box
         sx={{
+          width: "100%",
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "center",
+          marginBottom: "40px",
         }}
       >
-        <InputBase
-          startAdornment={
-            <InputAdornment
-              position="start"
-              sx={{
-                marginLeft: "10px",
-              }}
-            >
-              <SearchIcon />
-            </InputAdornment>
-          }
-          placeholder={t("homePage.namerOrNumber")}
-          inputProps={{ "aria-label": "name or number search" }}
+        <Box
           sx={{
-            ml: 1,
-            flex: 1,
-            width: "599.43px",
-            height: "35.18px",
-            background: "#EDEEF3",
-            borderRadius: "7px",
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            fontFamily: "Istok Web",
-            fontStyle: "normal",
-            fontWeight: 400,
-            fontSize: "18px",
-            lineHeight: "32px",
-            color: "#3A2F66",
-
-            "& input::placeholder": {
+            justifyContent: "space-between",
+          }}
+        >
+          <InputBase
+            startAdornment={
+              <InputAdornment
+                position="start"
+                sx={{
+                  marginLeft: "10px",
+                }}
+              >
+                <SearchIcon />
+              </InputAdornment>
+            }
+            placeholder={t("homePage.namerOrNumber")}
+            inputProps={{ "aria-label": "name or number search" }}
+            sx={{
+              ml: 1,
+              flex: 1,
+              width: "599.43px",
+              height: "35.18px",
+              background: "#EDEEF3",
+              borderRadius: "7px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
               fontFamily: "Istok Web",
               fontStyle: "normal",
               fontWeight: 400,
               fontSize: "18px",
               lineHeight: "32px",
-              color: "rgba(114, 107, 143, 0.45)",
+              color: "#3A2F66",
+
+              "& input::placeholder": {
+                fontFamily: "Istok Web",
+                fontStyle: "normal",
+                fontWeight: 400,
+                fontSize: "18px",
+                lineHeight: "32px",
+                color: "rgba(114, 107, 143, 0.45)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            }}
+            onKeyDown={(e) => {
+              if (e.code == "Enter") {
+                onSearch();
+              }
+            }}
+            onChange={(e) => {
+              dispatch(updatePokemonSearchValue(e.target.value));
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "36.84px",
+            height: "34.4px",
+            background: "#726B8F",
+            borderRadius: "7px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginLeft: "10px",
+          }}
+        >
+          <IconButton
+            sx={{ color: "white" }}
+            aria-label="apply filters"
+            onClick={() => setShowFilterArea(!showFilterArea)}
+          >
+            <TuneIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      {showFilterArea && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
               display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            },
-          }}
-          onKeyDown={(e) => {
-            if (e.code == "Enter") {
-              onSearch();
-            }
-          }}
-          onChange={(e) => {
-            dispatch(updatePokemonSearchValue(e.target.value));
-          }}
-        />
-      </Box>
-      <Box
-        sx={{
-          width: "36.84px",
-          height: "34.4px",
-          background: "#726B8F",
-          borderRadius: "7px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginLeft: "10px",
-        }}
-      >
-        <IconButton sx={{ color: "white" }} aria-label="apply filters">
-          <TuneIcon />
-        </IconButton>
-      </Box>
-    </Box>
+              justifyContent: "space-between",
+              flexDirection: "row",
+              width: "250px",
+            }}
+          >
+            <Typography
+              onClick={() => setFilterSection("type")}
+              sx={{
+                fontFamily: "Istok Web",
+                fontStyle: "normal",
+                fontWeight: 700,
+                fontSize: "20px",
+                lineHeight: "29px",
+                display: "flex",
+                alignItems: "center",
+                textalign: "center",
+                color: filterSection === "type" ? "#3A2F66" : "#8D99D6",
+                cursor: "pointer",
+              }}
+            >
+              Type
+            </Typography>
+            <Typography
+              onClick={() => setFilterSection("generation")}
+              sx={{
+                fontFamily: "Istok Web",
+                fontStyle: "normal",
+                fontWeight: 700,
+                fontSize: "20px",
+                lineHeight: "29px",
+                display: "flex",
+                alignItems: "center",
+                textalign: "center",
+                color: filterSection === "generation" ? "#3A2F66" : "#8D99D6",
+                cursor: "pointer",
+              }}
+            >
+              Generation
+            </Typography>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
 
